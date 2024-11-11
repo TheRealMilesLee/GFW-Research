536ea731bee2eb169c3d9cfe8a0d20713984f60e
TheRealMilesLee <grandtheftauto710@outlook.com>
2024-11-11 13:17:03 -0800

    Need to figure out a way to cleanup this mess

--- Database_analysis_AfterDomainChange.py
+++ Database_analysis_AfterDomainChange.py
@@ -13,3 +13,29 @@ logging.basicConfig(
 # 创建 logger
 logger = logging.getLogger(__name__)
 
+# Constants
+CM_DNSP_ADC = ADC_db['China-Mobile-DNSPoisoning']
+CM_GFWL_ADC = ADC_db['China-Mobile-GFWLocation']
+
+CT_DNSP_ADC = ADC_db['China-Telecom-DNSPoisoning']
+CT_GFWL_ADC = ADC_db['China-Telecom-GFWLocation']
+CT_IPB_ADC = ADC_db['China-Telecom-IPBlocking']
+
+UCD_GFWL_ADC = ADC_db['UCDavis-Server-GFWLocation']
+UCD_IPB_ADC = ADC_db['UCDavis-Server-IPBlocking']
+UCD_DNSP_ADC = ADC_db['UCDavis-Server-DNSPoisoning']
+
+# 对于China-Mobile-DNSPoisoning, 数据格式为id, domain, result。 针对每一个domain的result进行统计分析, 生成一个summary插入到当前domain的document中
+
+def process_CM_DNSP_ADC():
+    logger.info("Processing China-Mobile-DNSPoisoning")
+    for domain in CM_DNSP_ADC.find():
+        result = domain['result']
+        summary = {
+            "total": len(result),
+            "poisoned": len([r for r in result if r == "poisoned"]),
+            "clean": len([r for r in result if r == "clean"]),
+            "unknown": len([r for r in result if r == "unknown"])
+        }
+        CM_DNSP_ADC.update_one({"_id": domain["_id"]}, {"$set": {"summary": summary}})
+    logger.info("China-Mobile-DNSPoisoning Done")
