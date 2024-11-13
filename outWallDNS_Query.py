efd6be758e0e5bc5b7218b84ef780c9161bf4839
Hengyi Li <trdli@ucdavis.edu>
2024-11-13 09:30:26 -0800

    Update the outWallDNS List

--- outWallDNS_Query.py
+++ outWallDNS_Query.py
@@ -14,6 +14,7 @@ def read_csv(file_path):
 
 # 通过指定的 DNS 服务器查询域名
 def query_dns(domain, dns_server, query_type):
+    print(f"正在查询 {domain} 的 {query_type} 记录...")
     try:
         res = resolver.Resolver()
         res.nameservers = [dns_server]
@@ -24,6 +25,7 @@ def query_dns(domain, dns_server, query_type):
 
 # 写入查询结果到 CSV 文件
 def write_results_to_csv(results, output_file="./src/Import/CorrectIPResult.csv"):
+    print("正在写入结果到 CorrectIPResult.csv...")
     with open(output_file, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(["Domain", "DNS Server", "IP Address", "Type"])
@@ -34,25 +36,26 @@ def main():
     # 读取域名列表和 DNS 服务器列表
     domain_list = read_csv("./src/Import/domains_list.csv")
     dns_servers = get_dns_servers()
-
     results = []
+    batch_size = 8
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_query = {
-            executor.submit(query_dns, domain, dns_server, 'A'): (domain, dns_server)
-            for domain in domain_list
-            for dns_server in dns_servers[0]
-        }
+    with concurrent.futures.ThreadPoolExecutor(max_workers=batch_size) as executor:
+        for i in range(0, len(domain_list), batch_size):
+            batch = domain_list[i:i + batch_size]
+            future_to_query = {
+                executor.submit(query_dns, domain, dns_servers[0][0], 'A'): domain
+                for domain in batch
+            }
 
-        for future in concurrent.futures.as_completed(future_to_query):
-            domain, dns_server = future_to_query[future]
-            try:
-                result = future.result()
-                if result[2]:  # If there are IP addresses
-                    for ip in result[2]:
-                        results.append([result[0], result[1], ip, "IPv4"])
-            except Exception as exc:
-                print(f'{domain} generated an exception: {exc}')
+            for future in concurrent.futures.as_completed(future_to_query):
+                domain = future_to_query[future]
+                try:
+                    result = future.result()
+                    if result[2]:  # If there are IP addresses
+                        for ip in result[2]:
+                            results.append([result[0], result[1], ip, "IPv4"])
+                except Exception as exc:
+                    print(f'{domain} generated an exception: {exc}')
 
     # 写入结果到 output.csv
     write_results_to_csv(results)
