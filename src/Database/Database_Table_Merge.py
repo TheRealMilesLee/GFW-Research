08e58a8f676fcc9ccc198fadbfb230e7865f1d64
Hengyi Li <trdli@ucdavis.edu>
2024-11-21 13:09:20 -0800

    Add the basic structure, start merging the database

--- src/Database/Database_Table_Merge.py
+++ src/Database/Database_Table_Merge.py
@@ -1,3 +1,16 @@
+"""
+This file is meant to merge the database tables in the Mongodb.
+We have two databases, afterDomainChange and beforeDomainChange. In those database, we have three type of the collections:
+
+1. DNSPoisoning
+2. IPBlocking
+3. GFW Location
+
+The purpose of this file is to merge the tables in the two databases. The merging process is done by the domain name.
+So in the end, we wish we only have one database, with three table: DNSPoisoning, IPBlocking, GFW Location.
+
+The key of the merging would be base on domains. If the domain is the same, we will merge the data together.
+"""
 import concurrent.futures
 import csv
 import logging
